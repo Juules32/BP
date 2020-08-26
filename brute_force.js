65664099cae0e4aa306d7427fa47eaa47024a7a9
Benjamin Juul Jensen <BenjaminByvej@gmail.com>
2020-08-26 13:22:52 +0200

    working on algorithm

--- brute_force.js
+++ brute_force.js
@@ -1,6 +1,28 @@
-var password = 0
+var charSet = ["a", "b", "c"]
+answer = ""
+temp = ""
+console.log(answer)
 
 function login () {
-    password = document.getElementById("password").value
-    console.log(password)
+    
+    let password = document.getElementById("password").value
+    for (let j = 1; j <= 8; j++) {
+        for (let i = 0; i <= charSet.length ** j; i++) {
+            answer = charSet[i]
+            if (i == charSet.length ** j) {
+                temp.concat(temp, )
+            }
+            if (answer != password) {
+                continue
+            }
+            else {
+                console.log("Cracked!")
+                console.log(temp)
+                return
+            }
+        }
+        
+        j += 1
+    }
+   
 }
