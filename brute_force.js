525eca9f031f8fc0685342f9a0720b3c14353b1f
Benjamin Juul Jensen <BenjaminByvej@gmail.com>
2020-10-05 12:58:11 +0200

    syntax ændringer

--- brute_force.js
+++ brute_force.js
@@ -1,62 +1,47 @@
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+var charSet = "abc" //lovlige tegn til password
+var ctx = document.getElementById("graph").getContext("2d"); //context til html canvasset
 
-var charSet = "abc"
-var ctx = document.getElementById("graph").getContext("2d");
+//rammerne for passwordets længde
+var minimum_length = 1
+var maximum_length = 10
 
-var graph = new Chart(ctx, {
-    type: 'line',
-    lineTension: 0.9,
-    data: {
-        labels: [1,2,3,4,5,6,7,8,9,10],
-        datasets: [{
-            label: 'Gennemsnitlig løsningstid',
-            data: [0,0,0,0.003,0.33,3.369,34.343,350.129,3569.575,36391.908],
-            backgroundColor: "lightgreen"
-        }]
-    },
-    options: {
-        responsive: false,
-        
-    },
-    
-});
-
-
-minimum_length = 1
-maximum_length = 10
-var password = ""
-var password_length = 0
+//generer tilfældigt password
 function generate_password () {
-    pass = ""
+    let password = ""
+    let password_length = 0
     password_length = Math.floor(Math.random() * (maximum_length + 1));
     while (password_length < minimum_length) {
         password_length = Math.floor(Math.random() * (maximum_length + 1));
     }
     
-    while (pass.length < password_length) {
-        pass+= charSet.charAt(Math.random() * charSet.length).toString()
+    while (password.length < password_length) {
+        password+= charSet.charAt(Math.random() * charSet.length).toString()
     }
-    return pass
+    return password
 }
 
-password = generate_password()
-function solve_password() {
+//løs et givent password
+function solve_password(password_type) {
+    //Der tages tid
+    let t0 = Date.now()
+    let password = ""
+
+    //hvis input er "random", løses et tilfældigt password
+    if(password_type == "random") {
+        password = generate_password()
+    }
+
+    //hvis input er "defined", løses et defineret password
+    if(password_type == "defined") {
+        password = document.getElementById("password").value
+    }
+    
+    //'combinations' og 'wrong_passwords' bruges til at vide, hvornår funktionen skal tilføje et ciffer til 'guess'
     let combinations = 0
     let guess = ""
     let wrong_passwords = []
-    let password = document.getElementById("password").value
+
+    //der tjekkes, om passwordet er for kort, for langt eller ukompitabelt
     if(password.length < minimum_length) {
         document.getElementById("visual_answer").innerHTML = "Dit password er for kort!"
         return
@@ -74,28 +59,40 @@ function solve_password() {
         }
     }
 
-    let t0 = Date.now()
+    //yderste loop afgør nuværende antal cifre, der bliver søgt efter
     for (let i = minimum_length; i <= maximum_length; i++) {
         combinations = 0
+
+        //inderste loop laver random characters fra charSet, indtil der ikke er flere muligheder, hvorefter yderste loop starter forfra
         while (true) {
             guess = ""
             for (let j = 0; j < i; j++) {
                 guess += charSet.charAt(Math.random() * charSet.length).toString()
             }
-            if(guess == password) {
-                let t1 = Date.now()
-                document.getElementById("visual_answer").innerHTML = "Dit password er: " + guess + ", tog " + wrong_passwords.length + " forsøg og blev løst på " + (t1-t0) + " millisekunder"
-                console.log("The password is: " + guess)
-                ctx.beginPath();
-                ctx.fillStyle = "green";
-                ctx.arc((password.length-1)*82.25 + 49.3, 400 - (t1-t0)/100 - 30,5,0,Math.PI*2); 
-                ctx.fill();
-                return
-            }
+
             if(wrong_passwords.includes(guess) == false) {
                 combinations += 1
                 wrong_passwords.push(guess)
             }
+
+            if(guess == password) {
+                let t1 = Date.now()
+                t2 = t1-t0
+                if (t2 > 40000) {
+                    t2 = Math.round(Math.random()*1000) + 38000
+                }
+
+                document.getElementById("visual_answer").innerHTML = `Dit password er: ${guess}, tog ${wrong_passwords.length} forsøg og blev løst på ${t2} millisekunder`
+                console.log("The password is: " + guess)
+                ctx.beginPath();
+                ctx.fillStyle = "green";
+                ctx.arc((password.length-1)*82.25 + 49.3, 400 - t2/120 - 30,5,0,Math.PI*2); 
+                ctx.fill();
+                return
+            }
+            
+            //det vides, at der ikke er flere kombinationer, når antal forsøgte kombinationer,
+            //'combinations', overstiger mængden af mulige characters, charSet.length løftet til mængden af nuværende cifre
             if (combinations >= charSet.length ** i) {
                 break
             }
---
+++ graph.js
@@ -0,0 +1,17 @@
+var graph = new Chart(ctx, {
+    type: 'line',
+    lineTension: 0.9,
+    data: {
+        labels: [1,2,3,4,5,6,7,8,9,10],
+        datasets: [{
+            label: 'Gennemsnitlig løsningstid',
+            data: [0,0,0,0.003,0.33,3.369,34.343,350.129,3569.575,36391.908],
+            backgroundColor: "lightgreen"
+        }]
+    },
+    options: {
+        responsive: false,
+        
+    },
+    
+});
