fa8562fb7a7157eb27a55dd01ff5e5e999117079
Benjamin Juul Jensen <BenjaminByvej@gmail.com>
2020-10-07 12:26:32 +0200

    Last change

--- graph.js
+++ graph.js
@@ -5,7 +5,7 @@ var graph = new Chart(ctx, {
     data: {
         labels: [1,2,3,4,5,6,7,8,9,10],
         datasets: [{
-            label: 'Gennemsnitlig løsningstid (x er passwordlængde og y er tid i millisekunder',
+            label: 'Gennemsnitlig løsningstid (x er passwordlængde og y er tid i millisekunder)',
             data: [0,0,0,0.003,0.33,3.369,34.343,350.129,3569.575,36391.908],
             backgroundColor: "lightgreen"
         }]
